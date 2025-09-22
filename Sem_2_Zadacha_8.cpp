cee7f27437cf8be8e4cd36876547f8063c9cebd5
Artxmvl <talakhovartem2007@gmail.com>
2025-09-22 22:39:48 +0300

    Add files via upload

---
+++ Sem_2_Zadacha_8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    char a;
+    string n;
+    int count = 0;
+    cout << "Введите строку: ";
+    cin >> a;
+    while (cin.get(a) && a != '\n') {
+        n += a;
+        if (a == 'a' || a == 'A' || a == 'e' || a == 'E' || a == 'i' || a == 'I' || a == 'o' || a == 'O' || a == 'y' || a == 'Y' || a == 'u' || a == 'U') {
+            count += 1;
+        }
+    }
+    cout << "Ваше предложение: " << n;
+    cout << "Количество гласных букв в строке: " << count;
+    return 0;
+}
+

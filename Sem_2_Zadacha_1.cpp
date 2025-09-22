911f7b3971d8dee435ea5e3309fc276730d8c2e3
Artxmvl <talakhovartem2007@gmail.com>
2025-09-22 19:27:41 +0300

    Add files via upload

---
+++ Sem_2_Zadacha_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int dlina;
+    cout << "Введите длину в сантиметрах: ";
+    cin >> dlina;
+    cout << dlina / 100 << " метров " << dlina % 100 << " сантиметров";
+    return 0;
+}

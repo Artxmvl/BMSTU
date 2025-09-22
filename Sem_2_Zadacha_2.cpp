d1d28bd2b3f7c435d922d08b1c4b036e42cc33cf
Artxmvl <talakhovartem2007@gmail.com>
2025-09-22 19:38:36 +0300

    Add files via upload

---
+++ Sem_2_Zadacha_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int a = 0, b = 0;
+	cout << "Введите длину: "; 
+	cin >> a;
+	cout << "Введите ширину: ";
+	cin >> b;
+	cout << "Площадь прямоугольника равна: " << a * b;
+	return 0;
+}
+

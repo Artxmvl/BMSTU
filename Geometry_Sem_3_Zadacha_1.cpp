6b0e77eea7032faf1b3a9cf65746233ce4464c4e
Artxmvl <talakhovartem2007@gmail.com>
2025-09-23 21:26:32 +0300

    Add files via upload

---
+++ Geometry_Sem_3_Zadacha_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	double coord[6];
+	cout << "Введите массив из трех пар координат вершин: " << endl;
+	for (int i = 0;i < 6;i++) {
+		cin >> coord[i];
+	}
+	int x1 = coord[0], y1 = coord[1], x2 = coord[2], y2 = coord[3], x3 = coord[4], y3 = coord[5];
+
+	cout << "Площадь треугольника равна: " << abs (0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
+	return 0;
+}

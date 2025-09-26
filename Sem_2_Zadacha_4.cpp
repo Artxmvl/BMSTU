fbddc2d3fafeaf8141996ddd1138d9f3649f83bb
Artxmvl <talakhovartem2007@gmail.com>
2025-09-26 19:58:05 +0300

    Add files via upload

---
+++ Sem_2_Zadacha_4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int num = 0;
+    cout << "Введите целое число: ";
+    cin >> num;
+    if (num % 2 == 0) {
+        cout << "Число четное";
+    }
+    else {
+        cout << "Число нечетное";
+    }
+    return 0;
+}
+
+

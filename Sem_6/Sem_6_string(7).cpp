6786de826f4dcded3d7b89c964a504b2f8887245
Artxmvl <talakhovartem2007@gmail.com>
2025-10-18 20:37:48 +0300

    Add files via upload

---
+++ Sem_6/Sem_6_string(7).cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    string str;
+    cout << "Введите строку: " << endl;
+    getline(cin, str);
+    
+    //создание пустой строки для добавления элементов
+    string pust = "";
+
+    //добавдение параметров для повтора повторяющихся элементов
+    int count = 1;
+    char sumbols = str[0];
+    for (int i = 1;i <= str.size();i++) {
+        if (str[i] == sumbols) {
+            count++;
+        }
+        else {
+            pust += sumbols;
+            pust += to_string(count);
+
+
+            //сбравсывание счетчиков
+            count = 1;
+            sumbols = str[i];
+        }
+
+    }
+
+ 
+
+    //Вывод сжатой строки
+    cout << "Сжатая строка: " << pust << endl;
+    return 0;
+}
+

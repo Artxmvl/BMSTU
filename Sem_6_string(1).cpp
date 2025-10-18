55e72cdddf00248a947867e9c6a20aa2968231f9
Artxmvl <talakhovartem2007@gmail.com>
2025-10-18 14:18:24 +0300

    Add files via upload

---
+++ Sem_6_string(1).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    string stroka;
+    cout << "Введите строку: " << endl;
+    getline(cin, stroka);
+    
+    // удаление знаков препинания и пробелов для их игнорирования и перевод символов в нижний регистр
+    string cleaned;
+    for (char c : stroka) {
+        if (isalnum(c)) {
+            cleaned += tolower(c);
+        }
+    }
+
+    // разворот строки
+    
+
+    string reversed = cleaned;
+    reverse(reversed.begin(), reversed.end());
+
+
+    if (cleaned == reversed) {
+        cout << "Строка является палиндромом" << endl;
+    }
+    else {
+        cout << "Строка не является палиндромом" << endl;
+    }
+
+
+    return 0;
+}
+
+

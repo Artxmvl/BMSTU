590364d19f395ce6ed643471ffee649a0b480233
Artxmvl <talakhovartem2007@gmail.com>
2025-10-18 20:55:10 +0300

    Add files via upload

---
+++ Sem_6/Sem_6_string(8).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    string str;
+    cout << "Введите строку: " << endl;
+    getline(cin, str);
+
+    //начальная буква каждого предложения становится заглавной,остальные символы переходят в нижний регистр
+
+    str[0] = toupper(str[0]);
+    for (int i = 1;i < str.size();i++) {
+        if ((str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?') && isalpha(str[i])) {
+            str[i] = toupper(str[i]);
+        }
+        else {
+            str[i] = tolower(str[i]);
+        }
+    }
+
+    //вывод отредактированного текста
+    cout << "Отредактированный текст: " << str << endl;
+
+    return 0;
+}
+

40fb5cb2d29703cb5ee259099856ed4d99a1811c
Artxmvl <talakhovartem2007@gmail.com>
2025-10-18 19:52:24 +0300

    Add files via upload

---
+++ Sem_6_vector(6).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    string str;
+    cout << "Введите строку: " << endl;
+    getline(cin, str);
+    int shift;
+    cout << "Сдвиг: ";
+    cin >> shift;
+
+    //сдвиг вне зависимости от регистра
+    for (char &c : str) {
+        if (isupper(c)) {
+            c = (c - 'A' + shift) % 26 + 'A';
+        }
+        else if (islower(c)) {
+            c = (c - 'a' + shift) % 26 + 'a';
+        }
+    }
+
+    //вывод сдвинутого текста
+    cout << "Сдвинутый текст: " << str << endl;
+   
+    return 0;
+}
+

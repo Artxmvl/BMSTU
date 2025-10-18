7b74beeeeffc04caa7d7befb5797b5725244e676
Artxmvl <talakhovartem2007@gmail.com>
2025-10-18 18:37:23 +0300

    Add files via upload

---
+++ Sem_6/Sem_6_string(4).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    bool all_found = true;
+    string str1,str2;
+    cout << "Введите строку A: " << endl;
+    getline(cin, str1);
+    cout << "Введите строку B: " << endl;
+    getline(cin, str2);
+
+    //сравнение символов B с символами А
+    for (char c : str2) {
+        bool found = false;
+        //сравнение с элементами A
+        for (char b : str1) {
+            if (b == c) {
+                found = true;
+            }
+        }
+        //проверка результата
+        if (found == false) {
+            all_found = false;
+            break;
+        }
+    }
+    if (all_found == true) {
+        cout << "\nДА" << endl;
+    }
+    else {
+        cout << "\nНЕТ" << endl;
+    }
+ 
+    return 0;
+}

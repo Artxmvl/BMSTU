9bfef481b38c4e74561908fb14ea716b9d51dbb8
Artxmvl <talakhovartem2007@gmail.com>
2025-10-26 23:17:05 +0300

    Add files via upload

---
+++ Sem_7(ukazateli)_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int myStrLen(const char* str) {
+    int count = 0;
+    for (int i = 0; str[i] != '\0';i++) {
+            count++;
+    }
+    return count;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL,"RU");
+    char str[200];
+    cout << "Введите строку: " << endl;
+    cin.getline(str,200);
+    int length = myStrLen(str);
+    cout << "Длина строки равна: " << length;
+    return 0;
+}

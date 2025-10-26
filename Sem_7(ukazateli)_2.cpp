0b52c049c8638ad86679333886f4e2063e12ec8b
Artxmvl <talakhovartem2007@gmail.com>
2025-10-26 23:47:47 +0300

    Add files via upload

---
+++ Sem_7(ukazateli)_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void myStrCopy(char* dest,const char* src) {
+    int i ;
+    for (i = 0;src[i] != '\0';i++) {
+        dest[i] = src[i];
+        
+    }
+    dest[i] = '\0';
+
+}
+
+
+int main()
+{
+    setlocale(LC_ALL,"RU");
+    char start[200];//введенная строка
+    char end[200];//строка куда будем копировать
+    cout << "Введите строку: " << endl;
+    cin.getline(start, 200);
+
+    myStrCopy(end,start);
+
+    cout << "Скопированная строка: " << end << endl;
+    return 0;
+}

ccc28acb369a39d7212b6e28b0ce9ef796792aa0
Artxmvl <talakhovartem2007@gmail.com>
2025-10-27 00:14:32 +0300

    Add files via upload

---
+++ Sem_7(ukazateli)_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void myStrCat(char* dest, const char* src) {
+    int i;
+    for (i = 0;dest[i] != '\0';i++);
+    for (int j = 0;src[j] != '\0';j++) {
+        dest[i] = src[j];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    char start[200];//первая строка
+    char end[200];//строка которую добавят в конец первой
+    cout << "Введите первую строку: " << endl;
+    cin.getline(start, 400);
+    cout << "Введите вторую  строку: " << endl;
+    cin.getline(end, 200);
+
+    myStrCat(start, end);
+
+    cout << "Суммированная строка: " << start << endl;
+    return 0;
+}

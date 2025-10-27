32ae0988ee6e1f8ef42b20881d921d88d1297175
Artxmvl <talakhovartem2007@gmail.com>
2025-10-27 21:10:33 +0300

    Add files via upload

---
+++ Sem_7/Sem_7(ukazateli)_6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+void reverse(char* src) {
+    char* start = src;
+    char* end = src;
+    while (*end != '\0') {
+        end++;
+    }
+    end--; //минус один , так как последни    символ '\0'
+    
+    while (start < end) { //обмениваем значения указателей
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+
+        //сдигаем указатели
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    char str[200];
+    cout << "Введите строку: " << endl;
+    cin.getline(str, 200);
+    
+    reverse(str);
+    cout << "Развернутая строка: " << str << endl;
+
+    return 0;
+}

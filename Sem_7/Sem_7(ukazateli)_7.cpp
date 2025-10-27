1b7919dab0706c7ac449f0587807c8e3baaec1a5
Artxmvl <talakhovartem2007@gmail.com>
2025-10-27 21:52:12 +0300

    Add files via upload

---
+++ Sem_7/Sem_7(ukazateli)_7.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(const char* str) {
+    const char* start = str;
+    const char* end = str;
+    while (*end != '\0') {
+        end++;
+    }
+    end--;
+
+    while (start < end) {
+        if (*start != *end) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    char str[200];
+    cout << "Введите строку: " << endl;
+    cin.getline(str, 200);
+
+    bool result = isPalindrome(str);
+    if (result) {
+        cout << "Строка является палиндромом!" << endl;
+    }
+    else {
+        cout << "Строка не является палиндромом!" << endl;
+    }
+    return 0;
+}

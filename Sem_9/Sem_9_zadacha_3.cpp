dde22c41841aa643123d5c6f8fa61af292314584
Artxmvl <talakhovartem2007@gmail.com>
2025-11-03 00:57:13 +0300

    Add files via upload

---
+++ Sem_9/Sem_9_zadacha_3.cpp
@@ -0,0 +1,32 @@
+﻿#include <memory>
+#include <iostream>
+
+using namespace std;
+
+void fillArray(unique_ptr<int[]>& arr, int n) {
+    for (int i = 0;i < n;i++) {
+        arr[i] = i * i;
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int N = 0;
+    cout << "Введите размер масссива: ";
+    cin >> N;
+
+    unique_ptr<int[]> arr = make_unique<int[]>(N);
+    fillArray(arr, N);
+    
+
+    //вывод заполненного массива
+    cout << "Заполненный массив: " << endl;
+    for (int i = 0;i < N;i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

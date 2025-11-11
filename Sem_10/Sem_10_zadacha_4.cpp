1d96693a7ee37eb60cca53445280e0e1c871a002
Artxmvl <talakhovartem2007@gmail.com>
2025-11-12 00:30:03 +0300

    Add files via upload

---
+++ Sem_10/Sem_10_zadacha_4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"RU");
+
+    //создание файла
+    ofstream file("source.txt");
+
+    //проверка на открытие
+    if (file.fail()) {
+        cout << "Файл не открылся";
+    }
+
+    //заполнение файла
+    file << "ueigjibdflgjnb" << endl;
+    file << "9378" << endl;
+    file << "urfjkn ursojvi 398" << endl;
+    file.close();
+
+    string str;
+
+
+    //создание файла для копирования и счетчик для строк
+    ifstream readfile("source.txt");
+    ofstream copyfile("copy.txt");
+    
+    int strok = 0;
+
+    //заполнение файла копи содержимым первого файла и вывод скопированного файла
+    cout << "Вывод скопированного файла" << endl;
+    while (getline(readfile, str)) {
+        copyfile << str << endl;
+        cout << str << endl;
+        strok++;
+    }
+    //закрываем получившиеся файлы
+    readfile.close();
+    copyfile.close();
+
+
+   
+
+    return 0;
+}
+

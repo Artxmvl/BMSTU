8d6a1dde290d909f87fa3d91d33a2d6e968a4227
Artxmvl <talakhovartem2007@gmail.com>
2025-12-06 09:30:00 +0300

    Add files via upload

---
+++ Sem_12/Sem_12_zadacha_3(union).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+union DateTime {
+    struct {
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+    };
+};
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    DateTime dt;
+
+    cout << "Введите год: ";   cin >> dt.year;
+    cout << "Введите месяц: "; cin >> dt.month;
+    cout << "Введите день: ";  cin >> dt.day;
+    cout << "Введите час: ";   cin >> dt.hour;
+    cout << "Введите минуту: "; cin >> dt.minute;
+    cout << "Введите секунду: "; cin >> dt.second;
+
+    // Форматированный вывод
+    cout << "\nДата и время: "
+        << dt.year << "-"
+        << setw(2) << setfill('0') << dt.month << "-"
+        << setw(2) << setfill('0') << dt.day << " "
+        << setw(2) << setfill('0') << dt.hour << ":"
+        << setw(2) << setfill('0') << dt.minute << ":"
+        << setw(2) << setfill('0') << dt.second << endl;
+
+    
+
+    return 0;
+}

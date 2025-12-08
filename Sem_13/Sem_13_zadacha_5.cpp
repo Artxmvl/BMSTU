b889a7a90c09e3bd636a105db1543a2036d0544b
Artxmvl <talakhovartem2007@gmail.com>
2025-12-08 23:29:42 +0300

    Add files via upload

---
+++ Sem_13/Sem_13_zadacha_5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Publication {
+protected:
+    string title;
+
+public:
+    Publication(std::string t) : title(t) {}
+};
+
+class Book : public Publication {
+private:
+    string author;
+    int year;
+
+public:
+    Book(string t, string a, int y) : Publication(t), author(a), year(y) {}
+
+    void displayInfo() {
+        setlocale(LC_ALL, "RU");
+        cout << "Название: " << title
+            << ", Автор: " << author
+            << ", Год выпуска: " << year << std::endl;
+    }
+};
+
+int main() {
+    Book myBook("Преступление и наказание", "Фёдор Достоевский", 1866);
+    myBook.displayInfo();
+    return 0;
+}

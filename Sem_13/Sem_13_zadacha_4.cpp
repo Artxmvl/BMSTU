b5e9951de276921f990ef197682fa86c7e218963
Artxmvl <talakhovartem2007@gmail.com>
2025-12-08 23:26:47 +0300

    Add files via upload

---
+++ Sem_13/Sem_13_zadacha_4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double l, double w) : length(l), width(w) {}
+
+    double calculateArea() {
+        return length * width;
+    }
+
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    void displayInfo() {
+        setlocale(LC_ALL, "RU");
+        cout << "Длина: " << length
+            << ", Ширина: " << width
+            << ", Площадь: " << calculateArea()
+            << ", Периметр: " << calculatePerimeter() << std::endl;
+    }
+};
+
+int main() {
+    Rectangle rect(10.0, 5.0);
+    rect.displayInfo();
+    return 0;
+}

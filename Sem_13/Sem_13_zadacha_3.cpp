02eecdcec824ecca2b4dfa268da7f79f068a0a45
Artxmvl <talakhovartem2007@gmail.com>
2025-12-08 23:23:45 +0300

    Add files via upload

---
+++ Sem_13/Sem_13_zadacha_3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;;
+
+class Book {
+private:
+    string title;
+    string author;
+    int year;
+
+public:
+    Book(string t, string a, int y) : title(t), author(a), year(y) {}
+
+    void displayInfo() {
+        setlocale(LC_ALL,  "RU");
+        cout << "Название: " << title
+            << ", Автор: " << author
+            << ", Год выпуска: " << year << std::endl;
+    }
+};
+
+int main() {
+    Book myBook("Мастер и Маргарита", "Михаил Булгаков", 1966);
+    myBook.displayInfo();
+    return 0;
+}
